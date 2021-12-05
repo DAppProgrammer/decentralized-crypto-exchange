14072b919832d95887a3bb6dc95f5f9a41fff8e4
Paramjeet Singh <dapp.programmer@gmail.com>
2021-12-05 15:22:23 +0530

    Token transfer function and test cases

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,30 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import 'openzeppelin-solidity/contracts/utils/math/SafeMath.sol';
+
 contract Token {
-    string public name = "My Name";
+    using SafeMath for uint;
+
+    //variables
+    string public name = "Meta Token";
+    string public symbol = "META";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    //events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    constructor()   {
+        totalSupply = 1000000 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender,_to, _value);
+        return true;
+    }
 
 }
